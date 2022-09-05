7d39f3aa86dabd38bd8676f12772df7b3e6374a2
Arun Sirrpi <sinthanaisirrpi.arun@abc.net.au>
2022-09-05 13:48:15 +1000

    Initial Commit

---
+++ tvOSSwiftUIAccessibilityDebugging/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tvOSSwiftUIAccessibilityDebugging
+//
+//  Created by Arun Sinthanaisirrpi on 5/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tvOSSwiftUIAccessibilityDebugging/tvOSSwiftUIAccessibilityDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvOSSwiftUIAccessibilityDebuggingApp.swift
+//  tvOSSwiftUIAccessibilityDebugging
+//
+//  Created by Arun Sinthanaisirrpi on 5/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct tvOSSwiftUIAccessibilityDebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
