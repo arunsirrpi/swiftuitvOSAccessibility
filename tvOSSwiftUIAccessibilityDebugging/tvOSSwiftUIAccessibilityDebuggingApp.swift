a2f43ba105f9a3079d215878fb6a8b50698f102f
Arun Sirrpi <sinthanaisirrpi.arun@abc.net.au>
2022-09-05 14:28:21 +1000

    Added the files

--- tvOSSwiftUIAccessibilityDebugging/tvOSSwiftUIAccessibilityDebuggingApp.swift
+++ tvOSSwiftUIAccessibilityDebugging/tvOSSwiftUIAccessibilityDebuggingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct tvOSSwiftUIAccessibilityDebuggingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(liveStreams: Mock.liveAirContent)
         }
     }
 }
